a0828f768c417300c9a3dd2fd167fd2587b8877c
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-10 19:41:30 +0300

    fix: type from CustomCheckBox

--- src/components/Input/CheckBox.tsx
+++ src/components/Input/CheckBox.tsx
@@ -1,32 +1,21 @@
 import styled from "styled-components";
 
 export const CustomCheckBox = styled.input.attrs({type: "checkbox"})`
-  position: absolute;
-  z-index: -1;
-  opacity: 0;
-  display: inline-flex;
-  align-items: center;
-  user-select: none;
-  &::before {
-    content: '';
-    display: inline-block;
-    width: 20px;
-    height: 20px;
-    flex-shrink: 0;
-    flex-grow: 0;
-    border: 1px solid #adb5bd;
-    border-radius: 0.25em;
-    margin-right: 0.5em;
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: 50% 50%;
-  }
-  &:checked{
-    border-color: #0b76ef;
-    background-color: #0b76ef;
-  }
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
 `;
+const StyledCheckbox = styled.div`
+  display: inline-block;
+  width: 16px;
+  height: 16px;
+  background: ${(props: any) => (props.checked ? 'salmon' : 'papayawhip')}
+  border-radius: 3px;
+  transition: all 150ms;
+  
 
+  
+`
 export const LabelText = styled.span`
   font-size: 16px;
   line-height: 19px;
