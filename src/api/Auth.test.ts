dd055d5f51d0790c7de173c5a8b588cad3eaed17
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-11 10:56:36 +0300

    feat: add check success auth user test

--- src/api/Auth.test.ts
+++ src/api/Auth.test.ts
@@ -1,11 +1,14 @@
 import {authAPI} from "./Auth";
 
 
-test('check auth user', () => {
+test('check success auth user', () => {
   return authAPI.login('steve.jobs@example.com', 'password', true).then(data => {
     expect(data).toStrictEqual({
       id: authAPI.userId,
       login: 'steve.jobs@example.com',
     })
   });
+});
+test('the fetch fails with an error', () => {
+  return expect(authAPI.login('login', 'password', true)).rejects.toMatch("Пользователя login не существует");
 });
