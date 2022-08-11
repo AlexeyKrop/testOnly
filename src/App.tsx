9087148f946f644efafa60f2529ab8defc82da5e
EnzoK <65413959+AlexeyKrop@users.noreply.github.com>
2022-08-11 09:42:36 +0300

    feat: add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,14 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Login} from "./components/pages/Login/Login";
-import styled from "styled-components";
+
 import {authAPI} from "./api/Auth";
 import {Navigate, Route, Routes} from 'react-router-dom';
 import {Profile} from "./components/pages/Profile/Profile";
 import {Error404} from "./components/pages/Error404/Error404";
+import { StyledHeaderApp, StyledWrapperApp } from './components/pages/App/App';
 
-const StyledWrapperApp = styled.div`
-  min-height: 80vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-direction: column;
-`
-const StyledHeaderApp = styled.h1`
-  font-weight: 700;
-  font-size: 64px;
-  line-height: 78px;
-  color: #000000;
-  margin: 40px auto 0 auto;
-  text-align: center;
-`
+
 export type UserType = {
   id: string
   login: string
---
+++ src/components/pages/App/App.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledWrapperApp = styled.div`
+  min-height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`
+export const StyledHeaderApp = styled.h1`
+  font-weight: 700;
+  font-size: 64px;
+  line-height: 78px;
+  color: #000000;
+  margin: 40px auto 0 auto;
+  text-align: center;
+`
